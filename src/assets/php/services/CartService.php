2837d1adc8d5f677f1f8f4b35429105db0f75295
serenamisantone <84023323+serenamisantone@users.noreply.github.com>
2023-11-14 12:06:18 +0100

    Finito carrello lo giuuuuro

--- src/assets/php/services/CartService.php
+++ src/assets/php/services/CartService.php
@@ -20,28 +20,6 @@ class CartService
         //creo un carrello per utenti non autenticati
         if (!isset($_SESSION['cart'])) {
             $_SESSION['cart'] = new ShoppingCart();
-        } else {
-            //creo un carrello nel database per utenti autenticati
-            /*  if (isset($_SESSION['auth'])) {
-
-
-                  $userId = $_SESSION['auth']['user'];
-                  $result = $this->connection->query("INSERT INTO shopping_cart(user_id, is_open) values ({$userId}, 1) ");
-                  if (!$result) {
-                      Header("Location: error.php?Shopping_cart_non_inserita");
-                  } else {
-                      $result = $this->connection->query("SELECT LAST_INSERT_ID() ");
-                      if ($result) {
-                          $lastInsertId = $result->fetch_assoc(); // Recupera un array associativo
-                          $shoppingCartId = $lastInsertId['LAST_INSERT_ID()'];
-                          $this->createShoppingCartProducts($shoppingCartId);
-                          $cartProducts = $_SESSION['cart']->getProducts();
-                          foreach ($cartProducts as $productId => $quantity) {
-                              $this->connection->query("INSERT INTO `shopping_cart_product` (shopping_cart_id, product_id, added_quantity) values ({$shoppingCartId}, {$productId}, {$quantity});");
-                          }
-                      }
-                  }
-              }*/
         }
     }
     public function createShoppingCartProducts($shoppingCartId)
@@ -73,15 +51,15 @@ class CartService
         if (isset($_SESSION['auth'])) {
 
             $cartId = $_SESSION['auth']['cart']->getShoppingCartId();
-            $query='';
-          
+            $query = '';
+
             //se il prodotto è gia nel carrello incremento la quantità
-            $query="UPDATE shopping_cart_product SET added_quantity = added_quantity + $quantity WHERE product_id = {$productId} and shopping_cart_id= {$cartId}";
-           
-           $result = $this->connection->query($query);
+            $query = "UPDATE shopping_cart_product SET added_quantity = added_quantity + $quantity WHERE product_id = {$productId} and shopping_cart_id= {$cartId}";
+
+            $result = $this->connection->query($query);
             if ($result && $this->connection->affected_rows > 0) {
                 // Il prodotto era già nel carrello e l'aggiornamento ha avuto successo
-                return true; 
+                return true;
             } else {
                 // Il prodotto non era nel carrello, inseriscilo nel carrello
                 $insertQuery = "INSERT INTO shopping_cart_product (shopping_cart_id, product_id, added_quantity) 
@@ -89,20 +67,21 @@ class CartService
                 $insertResult = $this->connection->query($insertQuery);
 
                 if ($insertResult) {
-                    return true;// Il prodotto è stato inserito con successo nel carrello
+                    return true; // Il prodotto è stato inserito con successo nel carrello
                 } else {
-                    return false;// Gestisci l'errore nell'inserimento del prodotto
+                    return false; // Gestisci l'errore nell'inserimento del prodotto
                 }
             }
-            
+
         } else {
             $_SESSION['cart']->addProduct($productId, $quantity);
             return true;
         }
-       
+
     }
     function assignShoppingCart($userId)
     {
+
         //cerco il carrello dell'utente nel database
         $result = $this->connection->query("SELECT id from shopping_cart where shopping_cart.user_id = '{$userId}' AND shopping_cart.is_open= 1 ;");
         if ($result === false) {
@@ -118,7 +97,8 @@ class CartService
                 $cart->setIsOpen(true);
                 //$cart->setUser($user);
                 $cart->setShoppingCartId($data['id']);
-
+                //aggiorno il carrello con i prodotti in SESSION[cart]
+                $this->updateCart($data['id']);
                 //query per trovare i prodotti nel carrello
                 $productsQuery = $this->connection->query("SELECT * from shopping_cart_product where shopping_cart_product.shopping_cart_id = '{$cart->getShoppingCartId()}'; ");
                 if ($productsQuery == false) {
@@ -134,6 +114,7 @@ class CartService
                     }
                     //se il carrello nel database è vuoto vado a riprendere i prodotti in session(cart)
                     if ($productsQuery->num_rows == 0) {
+                        //creo un carrello per utenti non autenticati
                         $this->createShoppingCartProducts($cart->getShoppingCartId());
                     }
 
@@ -151,7 +132,7 @@ class CartService
     {
         if (isset($_SESSION['auth'])) {
 
-         
+
             $userId = $_SESSION['auth']['user'];
             $userId = $this->connection->real_escape_string($userId);
             $productId = $this->connection->real_escape_string($productId);
@@ -173,19 +154,20 @@ class CartService
     }
 
     public function getCartProducts()
-    { $products = array();
+    {
+        $products = array();
         if (isset($_SESSION['auth'])) {
             $cartId = $_SESSION['auth']['cart']->getShoppingCartId();
-         
+
             $result = $this->connection->query("SELECT * FROM shopping_cart_product where shopping_cart_product.shopping_cart_id='{$cartId}'");
             if (!$result) {
                 Header("Location: error.php?qualcosa_e_andato_storto");
             }
             $products = array();
             if ($result->num_rows > 0) {
-                
+
                 while ($cartProduct = $result->fetch_assoc()) {
-                   
+
                     $product = $this->productService->getProductById($cartProduct['product_id']);
                     $productWithQuantity = array(
                         'product' => $product,
@@ -197,7 +179,7 @@ class CartService
             }
         } else {
             $cart = $_SESSION['cart'];
-            
+
             $cartProducts = $cart->getProducts();
             foreach ($cartProducts as $productId => $quantity) {
                 $product = $this->productService->getProductById($productId);
@@ -210,7 +192,7 @@ class CartService
             }
 
         }
-      
+
         return $products;
 
     }
@@ -243,24 +225,52 @@ class CartService
 
     }
 
-    public function getPrice($productId){
+    public function getPrice($productId)
+    {
         $product = $this->productService->getProductById($productId);
-        if(isset($_SESSION['auth'])){ 
-        $cartId = $_SESSION['auth']['cart']->getShoppingCartId();
-        $result = $this->connection->query("SELECT added_quantity FROM shopping_cart_product where shopping_cart_product.shopping_cart_id='{$cartId}' and shopping_cart_product.product_id='{$productId}'");
-        if(!$result){
-            Header("Location: error.php?qualcosa_e_andato_storto");
-        }
-        if($result->num_rows>0){
-            $data=$result->fetch_assoc();
-            $price= $product->getPrice()*$data['added_quantity'];
-        }
-        
-        }else{
+        if (isset($_SESSION['auth'])) {
+            $cartId = $_SESSION['auth']['cart']->getShoppingCartId();
+            $result = $this->connection->query("SELECT added_quantity FROM shopping_cart_product where shopping_cart_product.shopping_cart_id='{$cartId}' and shopping_cart_product.product_id='{$productId}'");
+            if (!$result) {
+                Header("Location: error.php?qualcosa_e_andato_storto");
+            }
+            if ($result->num_rows > 0) {
+                $data = $result->fetch_assoc();
+                $price = $product->getPrice() * $data['added_quantity'];
+            }
+
+        } else {
 
             $quantity = $_SESSION['cart']->getProducts()[$productId];
-            $price= $product->getPrice()*$quantity;
+            $price = $product->getPrice() * $quantity;
         }
         return $price;
+    }
+
+    public function updateCart($cartId)
+    {
+        $tempCart = $_SESSION["cart"];
+        $cartProducts = $tempCart->getProducts();
+        foreach ($cartProducts as $productId => $quantity) {
+
+
+            $result = $this->connection->query("SELECT added_quantity FROM shopping_cart_product where shopping_cart_product.shopping_cart_id='{$cartId}' and shopping_cart_product.product_id='{$productId}'");
+            if (!$result) {
+                Header("Location: error.php?qualcosa_e_andato_storto");
+            }
+
+            //se ho gia un prodotto incremento la quantità
+            if ($result->num_rows > 0) {
+                $result = $this->connection->query("UPDATE shopping_cart_product SET added_quantity = added_quantity + $quantity WHERE product_id = {$productId}  and shopping_cart_id= {$cartId}");
+            } else {
+                if ($result->num_rows == 0) {
+                    $result = $this->connection->query("INSERT INTO shopping_cart_product (shopping_cart_id, product_id, added_quantity) 
+                VALUES ('{$cartId}', '{$productId}', '{$quantity}'); ");
+                }
+            }
+            if (!$result) {
+                Header("Location: error.php?carerello non aggiornato");
+            }
+        }
     }
 }
